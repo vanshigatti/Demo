081fdb4f9c7ecebf535ffc98f291d5b718e1a61a
vanshigatti <vanshigatti@gmail.com>
2026-02-11 14:47:18 +0530

    Updated Input.java class name

--- Input.java
+++ Input.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
+public class Input {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
 
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
-  }
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+    }
 }
