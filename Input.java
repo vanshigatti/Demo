691269b1039901778826b8788589f6f10db70a74
vanshigatti <vanshigatti@gmail.com>
2026-02-11 14:50:25 +0530

    Updated Input.java class name

--- Input.java
+++ Input.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-
 public class Input {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter username");
 
-        String userName = myObj.nextLine();
+        if (args.length == 0) {
+            System.out.println("No username provided.");
+            return;
+        }
+
+        String userName = args[0];
         System.out.println("Username is: " + userName);
     }
 }
